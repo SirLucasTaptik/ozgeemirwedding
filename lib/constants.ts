ac362b77c46003c6cd224be3c8a0c0f5a8cc3ff2
SirLucas <101318158+SirLucasTaptik@users.noreply.github.com>
2026-07-23 16:38:05 +0400

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,20 @@
+export const wedding = {
+  bride: "Özge",
+  groom: "Emir",
+
+  date: new Date("2026-09-20T18:30:00"),
+
+  venue: "Rixos Premium Dubai",
+
+  address: "Dubai, UAE",
+
+  googleMaps:
+    "https://maps.google.com",
+
+  hashtag: "#OzgeEmir",
+
+  heroTitle:
+    "We would be honored to celebrate our happiest day with you.",
+
+  music: "/music/romantic.mp3",
+};
