8ac8ef186dc258ed40c7142ce0c80af7b9b25bff
SirLucas <101318158+SirLucasTaptik@users.noreply.github.com>
2026-07-23 16:42:50 +0400

    Update Section.tsx

--- components/ui/Section.tsx
+++ components/ui/Section.tsx
@@ -1,1 +1,20 @@
+type Props = {
+  id?: string;
+  children: React.ReactNode;
+  className?: string;
+};
 
+export default function Section({
+  id,
+  children,
+  className = "",
+}: Props) {
+  return (
+    <section
+      id={id}
+      className={`relative py-28 md:py-36 ${className}`}
+    >
+      {children}
+    </section>
+  );
+}
