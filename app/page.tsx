9837e9728a00f10b5b168021558bdbe700f25c49
SirLucas <101318158+SirLucasTaptik@users.noreply.github.com>
2026-07-24 10:55:42 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,27 @@
-import Intro from "@/components/Intro";
-import Loader from "@/components/Loader";
-import MobileNav from "@/components/MobileNav";
-import MusicPlayer from "@/components/MusicPlayer";
-import ScrollProgress from "@/components/ScrollProgress";
-import MouseGlow from "@/components/MouseGlow";
-import BackToTop from "@/components/BackToTop";
-
 import Hero from "@/components/sections/Hero";
 import Countdown from "@/components/sections/Countdown";
 import Details from "@/components/sections/Details";
 import Schedule from "@/components/sections/Schedule";
 import Venue from "@/components/sections/Venue";
-import Weather from "@/components/sections/Weather";
 import RSVP from "@/components/sections/RSVP";
 import Gallery from "@/components/sections/Gallery";
 import Instagram from "@/components/sections/Instagram";
+import Weather from "@/components/sections/Weather";
 import Footer from "@/components/sections/Footer";
 
 export default function Home() {
   return (
-    <>
-      {/* Intro Animation */}
-      <Intro />
-
-      {/* Initial Loader */}
-      <Loader />
-
-      {/* Mobile Navigation */}
-      <MobileNav />
-
-      {/* Scroll Progress */}
-      <ScrollProgress />
-
-      {/* Mouse Glow */}
-      <MouseGlow />
-
-      <main className="relative overflow-x-hidden bg-[#FCFBF8] text-neutral-800">
-        <Hero />
-
-        <Countdown />
-
-        <Details />
-
-        <Schedule />
-
-        <Venue />
-
-        <Weather />
-
-        <RSVP />
-
-        <Gallery />
-
-        <Instagram />
-
-        <Footer />
-      </main>
-
-      {/* Floating Music Button */}
-      <MusicPlayer />
-
-      {/* Back To Top */}
-      <BackToTop />
-    </>
+    <main className="overflow-x-hidden bg-[#FCFBF8]">
+      <Hero />
+      <Countdown />
+      <Details />
+      <Schedule />
+      <Venue />
+      <RSVP />
+      <Gallery />
+      <Instagram />
+      <Weather />
+      <Footer />
+    </main>
   );
 }
