55f54e1a9b49c76cb54ed1f4fd2ee3d43555e45b
SirLucas <101318158+SirLucasTaptik@users.noreply.github.com>
2026-07-24 10:23:34 +0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://ozgeemir.com",
+      url: "https://ozgeemirwedding.com.tr",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
