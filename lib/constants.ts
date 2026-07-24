a7487539d95e832968ffadf4c7f2b916db08c723
SirLucas <101318158+SirLucasTaptik@users.noreply.github.com>
2026-07-24 11:24:36 +0400

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,29 +1,24 @@
-export const wedding = {
-  bride: "Özge",
+heroTitle:
+  "Ailelerimizin mutluluğu ile hayatımızın en özel gününü birlikte kutlamak için sizi aramızda görmekten büyük mutluluk duyarız.",
 
-  groom: "Emir",
+date: new Date("2026-09-20T18:30:00"),
 
-  heroTitle:
-    "Ailelerimizin mutluluğu ve sevgisiyle, hayatımızın en özel gününü birlikte kutlamak için sizi aramızda görmekten onur duyarız.",
+timezone: "Europe/Istanbul",
 
-  date: new Date("2026-09-20T18:30:00"),
+hashtag: "#OzgeEmir",
 
-  timezone: "Europe/Istanbul",
+music: "/music/romantic.mp3",
 
-  hashtag: "#OzgeEmir",
+venue: "A11 Hotel Bosphorus",
 
-  music: "/music/romantic.mp3",
+address:
+  "Sultantepe, Paşalimanı Cad. No:10, Üsküdar / İstanbul",
 
-  venue: "A11 Hotel Bosphorus",
+phone: "+90 216 711 11 11",
 
-  address:
-    "Sultantepe, Paşalimanı Cad. No:10, Üsküdar / İstanbul",
+googleMaps:
+  "https://www.google.com/maps/dir//A11+Hotel+Bosphorus,+Sultantepe,+Pa%C5%9Faliman%C4%B1+Cad.+No:10,+34550+%C3%9Csk%C3%BCdar%2F%C4%B0stanbul,+T%C3%BCrkiye/@25.0905084,55.1486213,14z/data=!4m8!4m7!1m0!1m5!1m1!1s0x14cab7e569daacb9:0xb642cfd916dc9d51!2m2!1d29.0172863!2d41.0288161?entry=ttu&g_ep=EgoyMDI2MDcyMS4wIKXMDSoASAFQAw%3D%3D",
 
-  phone: "+90 216 711 11 11",
+language: "tr",
 
-  googleMaps:
-    "https://maps.google.com/?q=A11+Hotel+Bosphorus+Uskudar",
-
-  language: "tr",
-
-  weather: false,
+weather: false,
